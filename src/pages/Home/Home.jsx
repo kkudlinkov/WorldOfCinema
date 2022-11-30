40732689a36bb6639c601318a088e3d6c22defc6
Николай <kkudlinkov@mail.ru>
2022-12-01 00:19:40 +0300

    FIX: Исправлены отступы footer. Также доделана форма обратной связи

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,15 +4,15 @@ import {Link} from "react-router-dom";
 
 const Home = () => {
     return (
-        <div className="home">
-             <div className='container'>
+        <div className='container'>
+            <div className="home">
                 <div className="home__inner">
                     <h1 className="home__title">Добро пожаловать на CinemaWorld, пользователь!</h1>
                     <p className="home__intro">Это ознакомительный проект в котором ты сможешь найти все нужные тебе
                         фильмы и узнать любую информацию по ним.
                         <br/>Так что не теряй времени и присутпай скорее!</p>
                     <Link className="buttonLink" to="./main">
-                        <button className="buttonStart"> Начать! </button>
+                        <button className="buttonStart"> Начать!</button>
                     </Link>
                 </div>
             </div>
